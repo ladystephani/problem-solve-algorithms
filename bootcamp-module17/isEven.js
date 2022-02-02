2695c966101c422d05687a43e88a81932763705d
Lady Gaga <jessiezheqi.li@gmail.com>
2022-02-01 18:28:41 -0800

    Add in bootcamp module to set up

---
+++ bootcamp-module17/isEven.js
@@ -0,0 +1,16 @@
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//sample input
+//num = 10
+console.log(isEven(10));
+
+//achieved output
+//true
+
+//module.exports = isEven;
